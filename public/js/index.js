88d11b729412f954d367946c5fbcf27eef34881b
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-06-01 13:24:10 +0100

    added navbar functionality

--- public/js/index.js
+++ public/js/index.js
@@ -161,29 +161,41 @@ hamburger.addEventListener('click', ()=>{
     
   }); 
   
-  (function($) { // Begin jQuery
-    $(function() { // DOM ready
+  (function($) {
+    $(function() {
       // If a link has a dropdown, add sub menu toggle.
       $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        // Close one dropdown when selecting another
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
+        if ($(window).width() < 1100) {
+          // For mobile devices, toggle the dropdown on click
+          e.preventDefault();
+          $(this).siblings('.nav-dropdown').slideToggle();
+        }
       });
+  
+      // Close one dropdown when selecting another
+      $('nav ul li .nav-link').click(function(e) {
+        if ($(window).width() < 1100) {
+          $(this).closest('li').siblings().find('.nav-dropdown').hide();
+        }
+      });
+  
       // Clicking away from dropdown will remove the dropdown class
       $('html').click(function() {
         $('.nav-dropdown').hide();
       });
+  
       // Toggle open and close nav styles on click
       $('#nav-toggle').click(function() {
         $('nav ul').slideToggle();
       });
+  
       // Hamburger to X toggle
       $('#nav-toggle').on('click', function() {
         this.classList.toggle('active');
       });
-    }); // end DOM ready
-  })(jQuery); // end jQuery
+    });
+  })(jQuery);
+  
 
   document.addEventListener("DOMContentLoaded", function() {
     const popupOverlays = document.querySelectorAll(".popup-overlay");
